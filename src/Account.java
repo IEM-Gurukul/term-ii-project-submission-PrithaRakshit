5d2b8e4a7685ec0e9e24c4be3a99afb0e0963bd3
PrithaRakshit <pritharakshit49@gmail.com>
2026-03-28 23:30:17 +0530

    added deposit method in Account

--- src/Account.java
+++ src/Account.java
@@ -7,3 +7,9 @@ public class Account {
         this.balance = balance;
     }
 }
+public void deposit(double amount) {
+    if (amount > 0) {
+        balance += amount;
+        System.out.println("Deposited");
+    }
+}
