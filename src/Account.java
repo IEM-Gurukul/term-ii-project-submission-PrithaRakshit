b035145242d701f1b3516baf45784a3d313e6b95
PrithaRakshit <pritharakshit49@gmail.com>
2026-03-28 23:15:21 +0530

    created Account class

--- src/Account.java
+++ src/Account.java
@@ -6,22 +6,4 @@ public class Account {
         this.id = id;
         this.balance = balance;
     }
-
-    public void deposit(double amount) {
-        if (amount > 0) {
-            balance += amount;
-        }
-    }
-
-    public void withdraw(double amount) {
-        if (amount <= balance) {
-            balance -= amount;
-        } else {
-            System.out.println("Insufficient funds");
-        }
-    }
-
-    public void display() {
-        System.out.println("ID: " + id + " Balance: " + balance);
-    }
 }
