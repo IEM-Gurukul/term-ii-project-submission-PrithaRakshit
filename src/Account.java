7cd33cc71c8418ea0abd25dc1a7dcba4dfa7de92
PrithaRakshit <pritharakshit49@gmail.com>
2026-03-29 00:20:33 +0530

    added basic account details

--- src/Account.java
+++ src/Account.java
@@ -1,9 +1,34 @@
 public class Account {
-    public int id;
-    public double balance;
+    
+    private int id;
+    private double balance;
 
+    
+    public Account() {
+        this.id = 0;
+        this.balance = 0.0;
+    }
+
+    
     public Account(int id, double balance) {
         this.id = id;
+        this.balance = balance;
+    }
+
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
         this.balance = balance;
     }
 }
