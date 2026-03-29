af907a1ee4eba40759e39c95b6ec004e6f54fbfd
PrithaRakshit <pritharakshit49@gmail.com>
2026-03-29 09:08:28 +0530

    adding bank system details

---
+++ src/banksystem.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class banksystem {
+    public ArrayList<Account> accounts = new ArrayList<>();
+
+    public void addAccount(Account acc) {
+        accounts.add(acc);
+    }
+}
+    
+
